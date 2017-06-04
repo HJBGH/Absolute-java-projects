58074eb97a2afc437b3433caba5c4e25c4484832
HB <hb@localhost.localdomain>
2017-06-04 14:38:40 +0930

    Finished project 2 of chapter 10, marks.txt is a test input file

--- chapter10projectsFileIO/Project2.java
+++ chapter10projectsFileIO/Project2.java
@@ -7,16 +7,23 @@ import java.io.FileInputStream;
 /**
  * You have a text file named marks.txt that stores the marks obtained by the students of a particular grade.
  * It is a text file of strings representing numbers of type int and each line stores one number. Write a program that will read the
- * file and display the highest and lowers marks on screen.
+ * file and display the highest and lowest marks on screen.
  *
  */
 public class Project2 {
+	
 	private static final String FILENAME = "marks.txt";
+	private static final int HIGHEST_POSSIBLE_GRADE = 100;
+	private static final int LOWEST_POSSIBLE_GRADE = 0;
+	
 	public static void main(String args[]){
+		int highest = LOWEST_POSSIBLE_GRADE;
+		int lowest = HIGHEST_POSSIBLE_GRADE;
+		int temp = -1; //Holding variable for read loop
 		Scanner inputStreamScanner = null;
 		try
 		{
-			inputStreamScanner = new Scanner(new FileInputStream("marks.txt"));
+			inputStreamScanner = new Scanner(new FileInputStream(FILENAME));
 		}
 		catch (FileNotFoundException fnfe) //fnfe being an acronym for FileNotFoundException
 		{
@@ -24,6 +31,14 @@ public class Project2 {
 			System.exit(1);
 		}
 		//we assume that the file is well formed, IE; matches the description given by the project outline. This isn't good practise but I've got like 12 of these to do.
-		inputStreamScanner.close();
+		while (inputStreamScanner.hasNextLine())
+		{
+			temp = inputStreamScanner.nextInt();
+			if(temp > highest)
+				highest=temp;
+			if(temp<lowest)
+				lowest=temp;
+		}
+		System.out.printf("Lowest: %d%nHighest: %d%n", lowest, highest);
 	}
 }
