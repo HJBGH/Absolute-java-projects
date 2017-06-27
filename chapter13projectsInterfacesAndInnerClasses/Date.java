f063ea937591c7291e98472399bcb95541aaf0ed
hb <HJBowland@protonmail.com>
2017-06-27 20:05:42 +1000

    Finished adding methods from a couple of other displays in chapter 4

--- chapter13projectsInterfacesAndInnerClasses/Date.java
+++ chapter13projectsInterfacesAndInnerClasses/Date.java
@@ -12,18 +12,26 @@ public class Date
 	private int day;
 	private int year; //a four digit number
 	
-	public Date(Date birthDate) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public void setYear(int newYear) {
-		// TODO Auto-generated method stub
+	//Copy constructor
+	public Date(Date aDate) {
+		if(aDate == null) //not a date
+		{
+			System.out.println("Fatal error");
+			System.exit(0);
+		}
 		
+		month = aDate.month; //Doesn't this cause a privacy leak?
+		day = aDate.day;
+		year = aDate.year;
 	}
 
-	public boolean precedes(Date deathDate) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean precedes(Date otherDate)
+	{
+		//this return statement is somewhat confusing at a glance.
+		return (( year < otherDate.year) || 
+			(year == otherDate.year && getMonth() <
+			otherDate.getMonth()) ||
+			(year == otherDate.year && month.equals(otherDate.month) && day < otherDate.day));
 	}
 	
 	public void setDate(int monthInt, int day, int year)
@@ -96,12 +104,130 @@ public class Date
 			int yearInput = keyboard.nextInt();
 			if (dateOK(monthInput, dayInput, yearInput))
 			{
-				setDate(monthInput, dayInput, yearInput)
+				setDate(monthInput, dayInput, yearInput);
 				tryAgain = false;
+				keyboard.close();
 			}
 			else
 				System.out.println("Illegal date. Re-enter input.");
 		}
 	}
+	
+	/**
+	 * the functions below are from display 4.9 and 4.13
+	 */
+	public void writeOutput()
+	{
+		System.out.println(month + " " + day + " " + year);
+	}
+	
+	public void setMonth(int monthNumber)
+	{
+		if ((monthNumber <= 0) || (monthNumber > 12))
+		{
+			System.out.println("Fatal error");
+			System.exit(0);
+		}
+		else
+			month = monthString(monthNumber);
+	}
+	
+	public void setDay(int day)
+	{
+		if((day <= 0) || (day > 31))
+		{
+			System.out.println("Fatal error");
+			System.exit(0);
+		}
+		else
+			this.day = day;
+	}
+	
+	public void setYear(int year)
+	{
+		if((year < 1000) || (year > 9999))
+		{
+			System.out.println("Fatal Error.");
+			System.exit(0);
+		}
+		else
+			this.year = year;
+	}
+	
+	public boolean equals(Date otherDate)
+	{
+		return ((month.equalsIgnoreCase(otherDate.month))
+				&& (day == otherDate.day) && (year == otherDate.year)); 
+	}
+	
+	private String monthString(int monthNumber) //HOLY FUCK an enum is perfect for this
+	{
+		switch (monthNumber)
+		{
+		case 1:
+			return "January";
+		case 2:
+			return "February";
+		case 3:
+			return "March";
+		case 4:
+			return "April";
+		case 5:
+			return "May";
+		case 6:
+			return "June";
+		case 7:
+			return "July";
+		case 8:
+			return "August";
+		case 9:
+			return "September";
+		case 10:
+			return "October";
+		case 11:
+			return "November";
+		case 12:
+			return "December";
+		default:
+			System.out.println("Fatal Error");
+			System.exit(0);
+			return "Error"; //the compiler is so god-damn needy
+		}
+	}
+	
+	public int getMonth() //USE AN ENUM FFFFFFFUUUUCKKKKKKK
+	{
+		if(month.equals("January"))//don't need to worry about cases
+			return 1;
+		else if (month.equals("February"))
+			return 2;
+		else if (month.equals("March"))
+			return 3;
+		else if (month.equals("April"))
+			return 4;
+		else if (month.equals("May"))
+			return 5;
+		else if (month.equals("June"))
+			return 6;
+		else if (month.equals("July"))
+			return 7;
+		else if (month.equals("August"))
+			return 8;
+		else if (month.equals("September"))
+			return 9;
+		else if (month.equals("October"))
+			return 10;
+		else if (month.equals("November"))
+			return 11;
+		else if (month.equals("December"))
+			return 12;
+		else
+		{
+			System.out.println("Fatal error");
+			System.exit(0);
+			return 0; //needy compiler
+		}
+	}
+	
 	
 }
