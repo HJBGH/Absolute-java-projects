618fc7abbec8ef7f9f9817732eb0ebc2c60464e1
hb <HJBowland@protonmail.com>
2017-06-27 19:11:43 +1000

    finished copying date class from Absolute Java display 4.11

--- chapter13projectsInterfacesAndInnerClasses/Date.java
+++ chapter13projectsInterfacesAndInnerClasses/Date.java
@@ -1,5 +1,7 @@
 package chapter13projectsInterfacesAndInnerClasses;
 
+import java.util.Scanner;
+
 /**
  * Copied straight out of Absolute Java fifth edition. This code is crap, it can't deal with exceptions
  *
@@ -54,9 +56,52 @@ public class Date
 		}
 	}
 
-	private boolean dateOK(String monthString, int day, int year) {
-		// TODO Auto-generated method stub
-		return false;
+	public void setDate(int year)
+	{
+		setDate(1, 1, year);
 	}
-
+	
+	private boolean dateOK(int monthInt, int dayInt, int yearInt) {
+		return ( (monthInt >= 1) && (monthInt <=12) &&
+				(dayInt >= 1) && (dayInt <= 31) &&
+				(yearInt >= 1000) && (yearInt <= 9999) );
+	}
+	
+	private boolean dateOK(String monthString, int dayInt, int yearInt) {
+		return ( monthOK(monthString) &&
+				(dayInt >= 1) && (dayInt <= 31) &&
+				(yearInt >= 1000) && (yearInt <= 9999) );
+	}
+	
+	private boolean monthOK(String Month)
+	{
+		return (month.equals("January") || month.equals("February") ||
+				month.equals("March") || month.equals("April") ||
+				month.equals("May") || month.equals("June") ||
+				month.equals("July") || month.equals("August") ||
+				month.equals("September") || month.equals("October") ||
+				month.equals("November") || month.equals("December") );
+	}
+	
+	public void readInput()
+	{
+		boolean tryAgain = true;
+		Scanner keyboard = new Scanner(System.in);
+		while (tryAgain)
+		{
+			System.out.println("Enter month, day, and year.");
+			System.out.println("Do not use a comma.");
+			String monthInput = keyboard.next();
+			int dayInput = keyboard.nextInt();
+			int yearInput = keyboard.nextInt();
+			if (dateOK(monthInput, dayInput, yearInput))
+			{
+				setDate(monthInput, dayInput, yearInput)
+				tryAgain = false;
+			}
+			else
+				System.out.println("Illegal date. Re-enter input.");
+		}
+	}
+	
 }
