5b7cfe9fcce542f05cb659db1cc161f7346e72df
hb <HJBowland@protonmail.com>
2017-08-05 23:00:35 +1000

    added stub file for simple gui calculator, thought i'd better commit something this week

---
+++ chapter17projectsSwingOne/GuiSimpleCalculator.java
@@ -0,0 +1,36 @@
+package chapter17projectsSwingOne;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * My response to project suggestion 3 of chapter 17 of the Absolute Java Textbook.
+ * Trying to implement MVC style pattern here.
+ * @author hb
+ *
+ */
+public class GuiSimpleCalculator {
+
+	private class Model
+	{
+		
+	}
+	
+	private class View
+	{
+		
+	}
+	
+	private class Controller
+	{
+		
+	}
+}
