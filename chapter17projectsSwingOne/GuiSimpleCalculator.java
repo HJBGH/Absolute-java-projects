7128eb3cec8276d01c13cbcd56d05799a0259185
hb <HJBowland@protonmail.com>
2017-08-09 22:10:22 +1000

    removed the stub mvc code I had, it's a damn calculator, what was I thinking. Added stub constructor code.

--- chapter17projectsSwingOne/GuiSimpleCalculator.java
+++ chapter17projectsSwingOne/GuiSimpleCalculator.java
@@ -1,6 +1,7 @@
 package chapter17projectsSwingOne;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,42 +10,41 @@ import java.util.Observable;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
  * My response to project suggestion 3 of chapter 17 of the Absolute Java Textbook.
- * Trying to implement MVC style pattern here.
+ * 
  * @author hb
  *
  */
-//TODO; rethink using MVC for a simple calculator. what was I thinking when I made this stub code last night?
-public class GuiSimpleCalculator {
 
-	private class Model extends java.util.Observable
+public class GuiSimpleCalculator extends JFrame implements ActionListener{
+	//window dimension constants
+	private static final int HEIGHT = 350;
+	private static final int WIDTH = 600;
+	private static final String WINDOW_NAME = "Swing Calculator";
+	
+	public static void main(String args[])
 	{
-		
+		GuiSimpleCalculator jimmy = new GuiSimpleCalculator();
+		jimmy.setVisible(true);
 	}
 	
-	private class View implements java.util.Observer
+	public GuiSimpleCalculator()
 	{
-
-		@Override
-		public void update(Observable arg0, Object arg1) {
-			// TODO Auto-generated method stub
-			
-		}
-		
+		super(WINDOW_NAME);
+		//set up frame
+		this.setSize(WIDTH, HEIGHT);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 	}
-	
-	private class Controller implements java.awt.event.ActionListener
-	{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
